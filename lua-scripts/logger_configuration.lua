da83e9ccf6193a732eff0b92db54a8e029b3c751
Poomkawin Sihanatkathakul <106644850+psihana-mon@users.noreply.github.com>
2022-09-16 14:30:06 +0700

    Add lua-scripts
    Add lua-scripts

---
+++ lua-scripts/logger_configuration.lua
@@ -0,0 +1,19 @@
+local Config = {
+    ENGINE_ENDPOINT = "https://exxon.nonamesec.com/engine",
+    PROXY_URL = "",
+    CONNECT_TIMEOUT = 100,
+    SEND_TIMEOUT = 500,
+    READ_TIMEOUT = 500,
+    KEEPALIVE = 60000,
+    BATCH_MAX_SIZE = 100,
+    RETRY_COUNT = 0,
+    FLUSH_TIMEOUT = 1,
+    PROCESS_DELAY = 0,
+    TYPE = 15,
+    INDEX = 18,
+    MAX_BODY_SIZE = 5000000,
+    MAX_CONCURRENT_PROCESSES = 100,
+}
+
+
+return Config
